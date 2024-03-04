3cb0de465d00b714738e0d09c493eaac75cdbe66
Tuani M Martinelli <martinellituani27@gmail.com>
2024-03-04 20:28:32 -0300

    new file

---
+++ models/db.js
@@ -0,0 +1,17 @@
+const Sequelize = require ('sequelize');
+
+const sequelize = new Sequelize ( "projeto1", "root", "Tuanitt27@", {
+    host: 'localhost', 
+    port: 3306,
+    dialect: 'mysql'
+});
+
+sequelize.authenticate()
+.then(function() {
+    console.log("Conexão com o banco de dados realizada com sucesso");
+}).catch(function() {
+    console.log("Erro: Conexão com o banco de dados não realizado com sucesso");
+});
+
+module.exports = sequelize;
+
